fcb41e2243a80ed22d8a61489c4c1a2c0675ca53
zilet123456 <zilet123456@gmail.com>
2022-08-26 19:34:56 +0900

    위치 변경

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Nav from './component/Nav';
 import Header from './component/Header';
 import Footer from './component/Footer';
 import Home from './component/Home';
-import About from './component/About';
+import About from './page/About';
 import Contact from './component/Contact';
 import Blog from './component/Blog';
 import PostPage from './component/Postpage';
